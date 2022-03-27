41a3d8e8e864a58a510bfaa2fc774a0967178da0
Whitegrave <Whitegrave@DESKTOP-ODR918R>
2022-03-27 12:58:55 -0400

    Add project files.

---
+++ MVC-SIS_UI/Controllers/AdminController.cs
@@ -0,0 +1,144 @@
+﻿using MVC_SIS_Data;
+using MVC_SIS_Models;
+using MVC_SIS_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_SIS_UI.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Majors()
+        {
+            var model = MajorRepository.GetAll();
+            return View(model.ToList());
+        }
+
+        // 2) Create a method in the Admin Controller for States. This method should return a view to lists all the existing states
+        [HttpGet]
+        public ActionResult States()
+        {
+            var model = StateRepository.GetAll();
+            return View(model.ToList());
+        }
+
+        // 5a) Create a GET method in the Admin controller for adding states (AddState). It should return a view with an empty state model(AddEditStateVM) as a parameter.
+        [HttpGet]
+        public ActionResult AddState()
+        {
+            return View(new AddEditStateVM());
+        }
+
+        // 5b) Create the POST method in the Admin controller for adding states (AddState)
+        [HttpPost]
+        public ActionResult AddState(AddEditStateVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            viewModel.currentState.StateAbbreviation.ToUpper();
+            StateRepository.Add(viewModel.currentState);
+            return RedirectToAction("States");
+        }
+
+        // 7a) Add a GET method in the Admin controller for DeleteState. The method should take a string parameter for StateAbbreviation
+        [HttpGet]
+        public ActionResult DeleteState(string id)
+        {
+            DeleteStateVM viewModel = new DeleteStateVM();
+            viewModel.currentState = StateRepository.Get(id);
+            return View(viewModel);
+        }
+
+        // 7b) Create the POST method in the Admin controller for deleting states (DeleteState)
+        // The method should redirect the user to the States list view upon the successful completion of the Delete operation
+        [HttpPost]
+        public ActionResult DeleteState(DeleteStateVM viewModel)
+        {
+            StateRepository.Delete(viewModel.currentState.StateAbbreviation);
+            return RedirectToAction("States");
+        }
+
+        // Chose to create edit functionality
+        [HttpGet]
+        public ActionResult EditState(string id)
+        {
+            AddEditStateVM viewmodel = new AddEditStateVM();
+            viewmodel.currentState = StateRepository.Get(id);
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult EditState(AddEditStateVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            StateRepository.Edit(viewModel.currentState);
+            return RedirectToAction("States");
+        }
+
+        [HttpGet]
+        public ActionResult AddMajor()
+        {
+            return View(new AddEditMajorVM());
+        }
+
+        [HttpPost]
+        public ActionResult AddMajor(AddEditMajorVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            MajorRepository.Add(viewModel.currentMajor.MajorName);
+            return RedirectToAction("Majors");
+        }
+
+        [HttpGet]
+        public ActionResult EditMajor(int id)
+        {
+            AddEditMajorVM viewmodel = new AddEditMajorVM();
+            viewmodel.currentMajor = MajorRepository.Get(id);
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult EditMajor(AddEditMajorVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            MajorRepository.Edit(viewModel.currentMajor);
+            return RedirectToAction("Majors");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteMajor(int id)
+        {
+            DeleteMajorVM viewModel = new DeleteMajorVM();
+            viewModel.currentMajor = MajorRepository.Get(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteMajor(DeleteMajorVM viewModel)
+        {
+            MajorRepository.Delete(viewModel.currentMajor.MajorId);
+            return RedirectToAction("Majors");
+        }
+
+    }
+}
---
+++ MVC-SIS_UI/Models/AddEditStateVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MVC_SIS_Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+// 4a) Create the AddEditState ViewModel
+namespace MVC_SIS_UI.Models
+{
+    public class AddEditStateVM : IValidatableObject
+    {
+        public State currentState { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            // 4b) We want to make sure that the StateName and StaveAbbreviation are not blank or null, so add to the Validate method to make sure this cannot happen.
+
+            // Error if null
+            if (currentState == null || currentState.StateName == "" || currentState.StateAbbreviation == null)
+            {
+                errors.Add(new ValidationResult("Please enter a State Name and Abbreviation",
+                    new[] { "currentState.StateName, currentState.StateAbbreviation null or empty" }));
+            }
+
+            // Error if not correct length
+            if (currentState.StateName.Length > 20 || currentState.StateAbbreviation.Length != 2)
+            {
+                errors.Add(new ValidationResult("State Name must be 20 letters or less, and Abbreviation must be 2 letters.",
+                    new[] { "currentState.StateName length, currentState.StateAbbreviation length" }));
+            }
+
+            // Error if name has invalid characters or too many spaces
+            int countSpaces = currentState.StateName.Count(x => x.ToString() == " ");
+            if (!Regex.IsMatch(currentState.StateName, @"^[a-zA-Z ]+$") || currentState.StateName[0].ToString() == " " || countSpaces > 1)
+            {
+                errors.Add(new ValidationResult("State Name must contain letters and a maximum of one space only.",
+                    new[] { "currentState.StateName characters or spaces" }));
+            }
+
+            // Error if abbreviation has invalid characters
+            if (!Regex.IsMatch(currentState.StateAbbreviation, @"^[a-zA-Z]+$"))
+            {
+                errors.Add(new ValidationResult("State Abbreviation must contain letters only.",
+                    new[] { "currentState.StateAbbreviation characters" }));
+            }
+
+            return errors;
+        }
+    }
+}
